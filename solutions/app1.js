527ce2aab4aace1093cc9629a656be10a67aef6f
Judit Orosz <orosz.judit@gmail.com>
2021-11-21 19:48:21 +0100

    JS4-001 solved

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,25 @@
+/*const converterPromise = new Promise((resolve, reject) => {
+        if (arr.every(item => typeof item === String)) {
+            resolve(arr.map((item) => item.toLocaleUpparCase()))
+        } else {
+            reject(Error('Not all elements are string type!'))
+        }
+    }
+    )
+;*/
 
-
+const converterPromise = (arr) => { 
+    return new Promise((resolve, reject) => {
+    if (arr.every(item => typeof item === 'string')) {
+        resolve(arr.map(item => item.toLocaleUpperCase()))
+    } else {
+        reject('Error: Not all elements are string type!')
+    }
+}
+)};
 
 
 
 
 // export default converterPromise;
+export default converterPromise;
